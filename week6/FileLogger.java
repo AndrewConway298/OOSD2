a2aceb4241440c9f770cf04a6f0efa35c137fe6d
AndrewConway298 <andrewconway298@gmail.com>
2017-03-14 14:01:08 +0000

    Week 6 Added

---
+++ week6/FileLogger.java
@@ -0,0 +1,20 @@
+public class FileLogger implements Logger{
+
+	private static FileLogger logger;
+
+	private FileLogger(){
+	}
+
+	public static FileLogger getLogger() {
+	if(logger == null){
+		logger = new FileLogger();
+	}
+		return logger;
+	}
+
+	public synchronized void log(String msg) {
+		FileUtil futil = new FileUtil();
+		futil.writeToFile("log.txt", msg, true, true);
+	}
+
+}
---
+++ week6/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		LoggerFactory factory = new LoggerFactory();
+		Logger logger = factory.getLogger();
+		logger.log("A message to the Log");
+	}
+
+}
